a75946053bd61c8efd91c2c3416be0cc73a3b193
zhangaibin <zhangaibin@atguigu.com>
2021-12-02 15:09:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * program: gitjava01
+ * author: 张爱斌
+ * create: 2021-12-02 15:03
+ * description:
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+        System.out.println("厉害了我的国");
+    }
+}
---
+++ src/main/java/com/atguigu/git/GitTest02.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * program: gitjava01
+ * author: 张爱斌
+ * create: 2021-12-02 15:07
+ * description:
+ **/
+public class GitTest02 {
+}
