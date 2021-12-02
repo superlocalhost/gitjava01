9ad99c23dc34c74b47a864b1c833360597dfed69
superlocalhost <76680360+superlocalhost@users.noreply.github.com>
2021-12-02 18:16:46 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,6 +16,9 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("master test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
+        
+        
 
     }
 }
